e2d077e722a9bcc088747fbfc2b72a9f1275534a
bgt yhn <rpb2525@rit.edu>
2020-09-02 18:38:00 -0400

    The dealer and the player are now dealt cards, the dealer is given one face up on face down. The player is given two cards and a total

--- 21.py
+++ 21.py
@@ -13,8 +13,19 @@ dealer_cards = []
 
 player_cards = []
 
-#dealer cards again
+#card dealing phase
+
+#dealer cards
 while len(dealer_cards) != 2:
     dealer_cards.append(random.randint(1, 11))
     if len(dealer_cards) == 2:
-        print("Dealer has: ", dealer_cards)
+        print("")
+        print("Dealer has: ", dealer_cards[0], " , X", sep=(""))
+        print("")
+
+#player cards
+while len(player_cards) != 2:
+    player_cards.append(random.randint(1, 11))
+    if len(player_cards) == 2:
+        print("player has has: ", player_cards[0], " , ", player_cards[1], " for a total of: ", player_cards[0] + player_cards[1],  sep=(""))
+        print("")
